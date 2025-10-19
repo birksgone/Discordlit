824d0ebf9bdea8e2eec5ec0c32361f63f19d9ff9
BirksG <birksgone@email.com>
2025-10-19 19:39:26 +0900

    change description

--- app.py
+++ app.py
@@ -12,11 +12,12 @@ st.sidebar.success("Select a tool above.")
 
 st.markdown(
     """
-    This is a collection of tools designed to help with Discord formatting and utilities.
+    This is a collection of tools designed to help with Empires & Puzzxes Discord formatting and utilities.
     
     **👈 Select a tool from the sidebar** to get started.
     
     ### Available Tools
+    - Emoji List: Browse and filter a custom list of emojis used in SGG Discord servers.
     - **Table Formatter**: Convert table data copied from Google Sheets into a Discord-friendly monospaced format.
     
     ---
